34cdf394729007379efc0efefca2e5d268330380
thomasisebeck <thomasisebeck@gmail.com>
2023-10-13 11:19:24 +0200

    finished expanded announcement style

--- web-project/src/components/Announcement/Announcement.js
+++ web-project/src/components/Announcement/Announcement.js
@@ -21,6 +21,7 @@ function Announcement(props) {
                         <div className={styles.img}>
                             <img alt="profile" src={props.profilePhoto}/>
                         </div>
+
                     </div>
                     <div>
                         <p className={styles.p}>{props.content.length > 300 ? props.content.substring(0, 300) + "..." : props.content}</p>
@@ -35,11 +36,12 @@ function Announcement(props) {
                 <div className={styles.BackPanel} onClick={() => setExpanded(false)}></div>
                 <div className={styles.ExpandedAnnouncement}>
                     <div className={styles.InnerContent}>
-                        <div className={styles.TopBar}>
+                        <div className={styles.TopBarExpanded}>
                             <h2 className={styles.h2}>{props.heading}</h2>
-                            <div className={styles.img}>
+                            <div className={`${styles.img} ${styles.expandedImg}`}>
                                 <img alt="profile" src={props.profilePhoto}/>
                             </div>
+                            <button className={styles.Button} onClick={() => setExpanded(false)}>X</button>
                         </div>
                         <div className={styles.expandedPara}>
                             <p className={styles.p}>{props.content}</p>
--- web-project/src/components/Feed/Feed.js
+++ web-project/src/components/Feed/Feed.js
@@ -19,7 +19,7 @@ const data = [
         module: "COS333",
         content: "Dear Students,\n" +
             "\n" +
-            "We have an exciting event coming up in our math course! We are delighted to announce a guest lecture by Sarah May, a renowned mathematician in the field of [specific field of mathematics]. The lecture, titled \"[Lecture Title],\" will take place on [Date] at [Time] in [Location/Online Platform]. This is a fantastic opportunity to gain insights from an expert in the field and expand your understanding of mathematics. We encourage all of you to attend and make the most of this unique learning experience.",
+            "We have an exciting event coming up in our math course! We are delighted to announce a guest lecture by Sarah May, a renowned mathematician in the field of Statistics. The lecture, titled \"Oh for the love of Maths,\" will take place on 24 May at 15:00 in the RautenBach hall. This is a fantastic opportunity to gain insights from an expert in the field and expand your understanding of mathematics. We encourage all of you to attend and make the most of this unique learning experience.",
         heading: "Guest Lecturer",
         profilePhoto: profile2,
         color: colors["COS333"].normal,
@@ -27,15 +27,9 @@ const data = [
     },
     {
         module: "WTW164",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a vulputate urna. Nam sit amet nisi vitae ligula pulvinar porttitor. Vestibulum vitae magna et ipsum scelerisque ultricies quis vitae justo. Donec ut ultricies purus. Morbi eget ligula tortor. Cras pulvinar lobortis libero non ullamcorper. Donec quis urna nec mi eleifend tincidunt. Nam id dictum elit.",
-        heading: "Attendance is shocking!",
-        profilePhoto: profile3,
-        color: colors["WTW164"].normal,
-        isWhite: colors["WTW164"].whiteText
-    },
-    {
-        module: "WTW164",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a vulputate urna. Nam sit amet nisi vitae ligula pulvinar porttitor. Vestibulum vitae magna et ipsum scelerisque ultricies quis vitae justo. Donec ut ultricies purus. Morbi eget ligula tortor. Cras pulvinar lobortis libero non ullamcorper. Donec quis urna nec mi eleifend tincidunt. Nam id dictum elit.",
+        content: "Oh, you thought you could breathe a sigh of relief, didn’t you? You thought you could just slide by, attending class when it suits you, ignoring my warnings, and that there would be no consequences. Well, buckle up, because you haven’t seen the last of me.\n" +
+            "\n" +
+            "Let me break some news for you: most of your peers, the ones who actually showed up and gave a damn, they passed the last test. Imagine that! They put in the effort, they listened, they learned, and they conquered. Now, what about you? Where were you? Probably hitting the snooze button, or maybe you were too busy with your oh-so-important social life to bother with mathematics. Well, your apathy ends now",
         heading: "You haven't seen the last of me!",
         profilePhoto: profile3,
         color: colors["WTW164"].normal,
@@ -43,7 +37,11 @@ const data = [
     },
     {
         module: "WTW164",
-        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a vulputate urna. Nam sit amet nisi vitae ligula pulvinar porttitor. Vestibulum vitae magna et ipsum scelerisque ultricies quis vitae justo. Donec ut ultricies purus. Morbi eget ligula tortor. Cras pulvinar lobortis libero non ullamcorper. Donec quis urna nec mi eleifend tincidunt. Nam id dictum elit.",
+        content: "Alright, listen up, because clearly, some of you haven't been paying attention. This is your angry lecturer speaking, and I've got something to say about this situation.\n" +
+            "\n" +
+            "I'm flabbergasted, folks. Completely flabbergasted. I stand in front of the class, armed with knowledge, enthusiasm, and a passion for teaching, and what do I get in return? Empty seats! Seriously? Is this some kind of joke?\n" +
+            "\n" +
+            "Let me break it down for you. This math course isn't just another class. It's a golden opportunity, a chance for you to grasp the intricacies of mathematics, to conquer the world of numbers, and what do you do? You skip it. You skip it like it's some optional party you can decide to attend or not. Well, newsflash: it's not optional.",
         heading: "Attendance is shocking!",
         profilePhoto: profile3,
         color: colors["WTW164"].normal,
